f2fa7d542b274ca3c2ddf71569bdde778c7bb204
Simon Chaffetz <simis1000@gmail.com>
2014-10-26 16:39:36 -0700

    Create options list on the fly

--- select.js
+++ select.js
@@ -1,5 +1,8 @@
 (function () {
-  var styleOptions = $(".style-option");
+  var styleOptionLinks = $(".style-option"),
+      styleOptionList = $("ul#style-options");
+
+  createOptionsList();
 
   $("input[type='radio']").on("change", function(e) {
     var selectedStyle = $(e.target).val();
@@ -7,10 +10,38 @@
   });
 
   function changeStyle (selectedStyle) {
-    var activeStyleLink = styleOptions.filter("link[rel='stylesheet']")[0],
-        selectedStyleLink = styleOptions.filter("." + selectedStyle)[0];
+    var activeStyleLink = styleOptionLinks.filter("link[rel='stylesheet']")[0],
+        selectedStyleLink = styleOptionLinks.filter("." + selectedStyle)[0];
 
     activeStyleLink.rel = "";
     selectedStyleLink.rel = "stylesheet";
+  }
+
+  function createOptionsList () {
+    var styleInputs = $.map(styleOptionLinks, function(style, idx) {
+      var styleName = $(style).data('name'),
+          styleNamePretty = _formatStyleName(styleName);
+          input = $("<input>"),
+          label = $("<label>");
+
+      input.attr({
+        type: 'radio', name: 'stylesheet', value: styleName, id: styleName
+      });
+      label.attr('for', styleName).text(styleNamePretty);
+
+      if (idx === 0) input.attr('checked', true);
+
+      return $("<li>").append(input).append(label)[0];
+    });
+
+    styleOptionList.append(styleInputs);
+  }
+
+  function _formatStyleName(styleName) {
+    var words = styleName.split(/[^\w]/);
+    return $.map(words, function(word) {
+      return word.charAt(0)
+        .toUpperCase() + word.substring(1);
+    }).join(" ");
   }
 })();
